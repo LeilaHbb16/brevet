03971e9647a8b8d291ea81214f8223889b68ec2d
Leila <habibileila27@gmail.com>
2024-04-11 21:43:29 +0200

    tokenization

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import os
 import json
 import pandas as pd
+import nltk
+from nltk.tokenize import word_tokenize
 
 def parcourir_repertoire(repertoire):
     # Liste pour stocker les DataFrames de chaque fichier JSON
@@ -14,13 +16,24 @@ def parcourir_repertoire(repertoire):
                     contenu = json.load(f)
                     # Création d'un DataFrame à partir des données JSON
                     df = pd.json_normalize(contenu)
-                    dfs.append(df)
+                    # Tokenisation du texte dans toutes les colonnes
+                    df_tokenise = df.applymap(tokeniser_texte)
+                    dfs.append(df_tokenise)
                 print()
 
     return dfs
+
+def tokeniser_texte(texte):
+    # Tokenisation des mots
+    tokens = word_tokenize(str(texte))
+    return tokens
+
+# télécharger les données nécessaires pour NLTK
+nltk.download('punkt')
 
 # Chemin du répertoire à parcourir
 repertoire_a_explorer = '../brevets_alternants'
 
 # Charger les données JSON dans une liste de DataFrames
 donnees = parcourir_repertoire(repertoire_a_explorer)
+
