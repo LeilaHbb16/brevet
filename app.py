cd6c2c9f53fca3a98aba878634ea06de8e4ce9cb
Leila <habibileila27@gmail.com>
2024-04-11 20:38:58 +0200

    chargement data

--- app.py
+++ app.py
@@ -1,21 +1,26 @@
 import os
 import json
-from pprint import pprint
+import pandas as pd
 
 def parcourir_repertoire(repertoire):
+    # Liste pour stocker les DataFrames de chaque fichier JSON
+    dfs = [] 
     for dossier, sous_repertoires, fichiers in os.walk(repertoire):
         for fichier in fichiers:
             if fichier.endswith('.json'):
                 chemin_fichier = os.path.join(dossier, fichier)
-                print(f"Contenu du fichier {chemin_fichier}:")
+                print(f"Lecture du fichier {chemin_fichier}:")
                 with open(chemin_fichier, 'r') as f:
                     contenu = json.load(f)
-                    # Imprimer de manière plus lisible
-                    pprint(contenu)
+                    # Création d'un DataFrame à partir des données JSON
+                    df = pd.json_normalize(contenu)
+                    dfs.append(df)
                 print()
+
+    return dfs
 
 # Chemin du répertoire à parcourir
 repertoire_a_explorer = '../brevets_alternants'
 
-# Appel de la fonction pour parcourir le répertoire
-parcourir_repertoire(repertoire_a_explorer)
+# Charger les données JSON dans une liste de DataFrames
+donnees = parcourir_repertoire(repertoire_a_explorer)
