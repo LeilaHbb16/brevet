236bc490a6b725fd73c599c2407e9c239af66564
Leila <habibileila27@gmail.com>
2024-04-12 21:50:38 +0200

    feat wordEmb

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 import json
 import nltk
 import string
+import numpy as np
 import pandas as pd
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
@@ -10,22 +11,6 @@ from nltk.stem import WordNetLemmatizer
 # Les données nécessaires pour NLTK
 nltk.download('stopwords')
 nltk.download('punkt')
-
-# def parcourir_repertoire(repertoire):
-#     dfs = [] 
-#     for dossier, sous_repertoires, fichiers in os.walk(repertoire):
-#         for fichier in fichiers:
-#             if fichier.endswith('.json'):
-#                 chemin_fichier = os.path.join(dossier, fichier)
-    
-#                 with open(chemin_fichier, 'r') as f:
-#                     contenu = json.load(f)
-#                     # Création d'un DataFrame à partir des données JSON
-#                     df = pd.json_normalize(contenu)
-#                     # Tokenisation du texte dans toutes les colonnes
-#                     df_tokenise = df.applymap(tokeniser_texte)
-#                     dfs.append(df_tokenise)    
-#     return dfs
 
 def parcourir_repertoire(repertoire, cles_a_supprimer):
     dfs = [] 
@@ -64,9 +49,24 @@ def tokeniser_texte(texte):
    
     return tokens
 
-
 # Chemin du répertoire à parcourir
 repertoire_a_explorer = '../brevets_alternants'
 
 # Charger les données JSON dans une liste de DataFrames
 donnees = parcourir_repertoire(repertoire_a_explorer, cles_a_supprimer)
+
+def load_embeddings(file_path):
+    embeddings_index = {}
+    with open(file_path, encoding='utf-8') as f:
+        for line in f:
+            values = line.split()
+            word = values[0]
+            coefs = np.asarray(values[1:], dtype='float32')
+            embeddings_index[word] = coefs
+    return embeddings_index
+
+# Chemin vers le fichier d'embeddings GloVe
+embedding_file_path = './wordEmb/glove.6B.100d.txt'
+
+# Charger les embeddings
+embeddings_index = load_embeddings(embedding_file_path)
